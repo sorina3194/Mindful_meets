6c8b825675a3c5d1c23299c2774a4c7838fc5e76
sorina tudor <sorinatudor94@gmail.com>
2023-08-27 19:07:48 +0200

    testing pushes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+sorina = User.create(
+  email: "sorina@test.com",
+  password: "123456"
+)
