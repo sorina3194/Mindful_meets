cc54f501b8ac1cd85f0f28853e50718aacf792e8
sorina tudor <sorinatudor94@gmail.com>
2023-08-28 23:23:07 +0200

    routes to be confirmed by ta

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,41 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :friendships, only: [:show, :new, :create, :destroy]
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # friendships show
+  # friendships new ??
+  # friendships create
+  # friendships destroy
+
+
+  resources :chatSessions, only: [:new, :create, :index,:show]
+
+  # chatsessions new
+  # chatsessions create
+  # chatsessions show
+  # OR LIKE THIS:
+
+  resources :chatSessions do
+    resources :invitations, only: [:new, :create, :index, :show]
+  end
+  # invitations new and create
+  # invitations show sent and received
+  # invitations destroy
+
+
+  resources :feedbacks, only: [:new, :create]
+
+  # feedbacks new
+  # feedbacks create
+
+  resources :users, only: [:show, :update]
+
+  # users show
+  # users update
+
+  resources :privateChats, :only [:show, :create]
+
+  # privateChats show
+  # privateChats create
 end
