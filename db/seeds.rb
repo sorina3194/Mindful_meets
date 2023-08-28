1f7941f1725566571a91a831651a17c1230700d6
zuzannadobak <mail@zuzannadobak.com>
2023-08-28 16:11:04 +0200

    add detailed seed part2

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require 'faker'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +6,29 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+puts 'Creating 20 fake users...'
+20.times do
+  user = User.new(
+    # avatar: Faker::Avatar.image,
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    birthday: Faker::Date.birthday(min_age: 18, max_age: 60),
+    username: Faker::Internet.username,
+    email: Faker::Internet.email,
+    password: "123456",
+    min_age: rand(20..30),
+    max_age: rand(50..60),
+    biography: Faker::Lorem.sentences(number: 2),
+    skills: Faker::Lorem.words(number: 4),
+    city: Faker::Address.city,
+    hobbies: Faker::Hobby.activity,
+    music_genres: Faker::Music.genre,
+    book_genres: Faker::Book.genre,
+    movie_genres: Faker::Movie.title,
+    favorite_animal: Faker::Creature::Animal
+  )
+  user.save!
+end
+puts 'User seeds user are planted 🌱🌱🌱!'
