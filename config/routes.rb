cecbaf1293dd0835056028379f3f76dc72ed3fc3
zuzannadobak <mail@zuzannadobak.com>
2023-08-30 14:29:39 +0200

    connect display user profile works, basic = part I

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
     # registrations: 'users/registrations'
   }
   root to: "pages#home"
+  get "show_profile/:id", to: "pages#show_profile", as: "showprofile"
   resources :friendships, only: %i[index create destroy]
   resources :chat_sessions, only: %i[create index show]
 
@@ -11,9 +12,7 @@ Rails.application.routes.draw do
     resources :invitations, only: %i[new create index show destroy]
   end
 
-
   # TODO: Chat
   # resources :private_chats, only: %i[show create]
   # resources :feedbacks, only: %i[new create]
-
 end
