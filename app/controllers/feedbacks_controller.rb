c765d062619a515f74bc9625d1d2a7e49af2c1fc
sorina tudor <sorinatudor94@gmail.com>
2023-09-04 11:19:37 +0200

    current user cant start a new session if one is in progress

--- app/controllers/feedbacks_controller.rb
+++ app/controllers/feedbacks_controller.rb
@@ -1,5 +1,21 @@
 class FeedbacksController < ApplicationController
   def new
-    
+    @feedback = Feedback.new
+  end
+  
+  def create
+    @feedback = Feedback.new(feedback_params)
+    if @feedback.save
+      flash[:success] = 'Feedback created successfully'
+      redirect_to root_path # You can redirect to any desired path
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def feedback_params
+    params.require(:feedback).permit(:content, :target_user_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,13 +5,17 @@ Rails.application.routes.draw do
     # registrations: 'users/registrations'
   }
   root to: "pages#home"
-  post "link_generate/:id", to: "chat_sessions#link_generate", as: "generate_link"
   get "show_profile/:id", to: "pages#show_profile", as: "showprofile"
+  # patch "finish/:id", to: "chat_sessions#finish", as: "finishchat"
   resources :friendships, only: %i[index create destroy]
   resources :feedbacks, only: %i[new create]
   resources :invitations, only: :index
 
+
   resources :chat_sessions, only: %i[index show create] do
+    member do
+      patch :finish
+    end
     resources :invitations, only: %i[new create index show destroy] do
       member do
         patch :accept
