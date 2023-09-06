dee649fb6c44b527425544cef23a438af2ff442b
sorina tudor <sorinatudor94@gmail.com>
2023-09-06 14:39:57 +0200

    conversations page displayed through feedback action

--- app/models/chat_session.rb
+++ app/models/chat_session.rb
@@ -1,7 +1,9 @@
 class ChatSession < ApplicationRecord
   has_many :invitations
 
-  validates :status, presence: true, inclusion: { in: %w[pending active completed] }
+  enum status: { pending: "pending", active: "active", completed: "conpleted" }
+
+  # validates :status, presence: true, inclusion: { in: %w[pending active completed] }
   # validates :video_chat_link, presence: true
 
   # Add counter cache to track invitations count
